2041f2daa1df73aede05190e67660ec387f1f158
douglasramosdev <121096421+douglasramosdev@users.noreply.github.com>
2024-02-24 12:04:29 -0300

    yes

--- exercicio3/src/App.java
+++ exercicio3/src/App.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        @SuppressWarnings("resource")
+        Scanner scanner = new Scanner(System.in);
+        Double valorX;
+        Double valorY;
+        char operadorLogico;
+        Double resultado;
+
+        System.out.println("Digite um valor: ");
+        valorX = scanner.nextDouble();
+        System.out.println("Informe a operação ( (+) , (-) , (*) , (/) ): ");
+        operadorLogico = scanner.next().charAt(0);
+        System.out.println("Digite outro valor: ");
+        valorY = scanner.nextDouble();
+
+
+        if(operadorLogico == '+'){
+            resultado = valorX + valorY;
+            System.out.println(resultado);
+        }else if (operadorLogico == '-'){
+            resultado = valorX - valorY;
+            System.out.println(resultado);
+        }else if (operadorLogico == '*'){
+            resultado = valorX * valorY;
+            System.out.println(resultado);
+        }else if (operadorLogico == '/'){
+            resultado = valorX / valorY;
+            System.out.println(resultado);
+        } else {
+                System.out.println("informe um operador logico valido!!");
+        }
     }
 }
