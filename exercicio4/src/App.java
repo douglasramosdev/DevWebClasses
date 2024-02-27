ebc4b4960b3869451c99357df77d663dde4d36fe
douglasramosdev <121096421+douglasramosdev@users.noreply.github.com>
2024-02-26 22:00:32 -0300

    commit

--- exercicio4/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        int i = 1;
-        for (i = 1; i < 1001; i++){
-            System.out.println(i);
-        }
-    }
-}
